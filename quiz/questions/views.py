483ad78d20adfab162cc1c3db54b0b6a1311cca5
Priyanshu2sh <ps200410@gmail.com>
2024-01-12 22:58:12 +0530

    Resolved Issues

--- quiz/questions/views.py
+++ quiz/questions/views.py
@@ -4,6 +4,7 @@ from .forms import RegistrationForm
 from django.contrib import messages
 import os
 from twilio.rest import Client
+import random
 # Create your views here.
 
 def registration(request):
@@ -42,38 +43,37 @@ def registration_otp(request):
     global user_name
     global user_email
     global user_mobile
-    if str(user_mobile) in request.session:
-        account_sid = "ACbf04a8f8f4e4f25e7eda3310e7a7b17f"
-        auth_token = "291298433eb51fa82e68433a2185846e"
-        verify_sid = "VA9920ccda8a38f9924baffdae6e89d434"
-        mobile=request.session[str(user_mobile)]
-        verified_number = "+91"+str(mobile)
-        client = Client(account_sid, auth_token)
-        if request.method=='POST':
-            otp_code = request.POST.get('otp')
-
-            verification_check = client.verify.v2.services(verify_sid) \
-                .verification_checks \
-                .create(to=verified_number, code=otp_code)
-            if verification_check.status == 'approved':
-                user_name=request.session[str(user_name)]
-                user_email=request.session[str(user_email)]
-                user_mobile=request.session[str(user_mobile)]
-                user=Registration(name=user_name,email=user_email,mobile=user_mobile)
-                user.save()
-                messages.success(request,'Verified Successfully')
-                return HttpResponseRedirect('/home')
-
+    global otp
+    if request.method=='POST':
+        otp_code = request.POST.get('registration_otp')
+        if str(otp) == otp_code:
+            print(True)
+            user_name=request.session[str(user_name)]
+            user_email=request.session[str(user_email)]
+            user_mobile=request.session[str(user_mobile)]
+            user=Registration(name=user_name,email=user_email,mobile=user_mobile)
+            user.save()
+            messages.success(request,'Verified Successfully')
+            return HttpResponseRedirect('/home')
         else:
-
-            verification = client.verify.v2.services(verify_sid) \
-                .verifications \
-                .create(to=verified_number, channel="sms")
-            print(verification.status)
-            return render(request,'questions/registration_otp.html')
+            pass
     else:
-        messages.warning(request,'Login First')
-        return HttpResponseRedirect('/login')
+        if str(user_mobile) in request.session:
+            mobile=request.session[str(user_mobile)]
+            verified_number = "+91"+str(mobile)
+            otp=random.randint(100000,999999)
+            account_sid="ACbf04a8f8f4e4f25e7eda3310e7a7b17f"
+            auth_token="291298433eb51fa82e68433a2185846e"
+            client=Client(account_sid,auth_token)
+            msg=client.messages.create(
+                    body = f"Your otp is: {otp}",
+                    from_ = "+15305534598",
+                    to = verified_number
+                )
+            return render(request,'questions/registration_otp.html')
+        else:
+            messages.warning(request,'Login First')
+            return HttpResponseRedirect('/login')
 
 
 def login(request):
@@ -96,69 +96,81 @@ def login(request):
     
 def login_otp(request):
     global user_mobile
-    if str(user_mobile) in request.session:
-        account_sid = "ACbf04a8f8f4e4f25e7eda3310e7a7b17f"
-        auth_token = "291298433eb51fa82e68433a2185846e"
-        verify_sid = "VA9920ccda8a38f9924baffdae6e89d434"
-        mobile=request.session[str(user_mobile)]
-        verified_number = "+91"+str(mobile)
-        client = Client(account_sid, auth_token)
-        if request.method=='POST':
-            otp_code = request.POST.get('otp')
-
-            verification_check = client.verify.v2.services(verify_sid) \
-                .verification_checks \
-                .create(to=verified_number, code=otp_code)
-            if verification_check.status == 'approved':
-                messages.success(request,'Logged in Successfully')
-                return HttpResponseRedirect('/home')
-
-        else:
-
-            verification = client.verify.v2.services(verify_sid) \
-                .verifications \
-                .create(to=verified_number, channel="sms")
-            print(verification.status)
-            return render(request,'questions/login_otp.html')
+    global otp
+    
+    if request.method=='POST':
+        otp_code = request.POST.get('login_otp')
+        if otp_code == str(otp):
+            messages.success(request,'Logged in Successfully')
+            return HttpResponseRedirect('/home')
     else:
-        messages.warning(request,'Login First')
-        return HttpResponseRedirect('/login')
+        if str(user_mobile) in request.session:
+            mobile=request.session[str(user_mobile)]
+            verified_number = "+91"+str(mobile)
+            otp=random.randint(100000,999999)
+            account_sid="ACbf04a8f8f4e4f25e7eda3310e7a7b17f"
+            auth_token="291298433eb51fa82e68433a2185846e"
+            client=Client(account_sid,auth_token)
+            msg=client.messages.create(
+                body = f"Your otp is: {otp}",
+                from_ = "+15305534598",
+                to = verified_number
+                )
+            return render(request,'questions/login_otp.html')
+        else:
+            messages.warning(request,'Login First')
+            return HttpResponseRedirect('/login')
 
 
 def home(request):
     global user_mobile
-    if str(user_mobile) in request.session:
-        courses=Course.objects.all()
-        return render(request, 'questions/home.html',{'courses':courses})
-    else:
+    try:
+        if str(user_mobile) in request.session:
+            courses=Course.objects.all()
+            return render(request, 'questions/home.html',{'courses':courses})
+        else:
+            messages.warning(request,'Login First')
+            return HttpResponseRedirect('/login')
+    except NameError as e:
         messages.warning(request,'Login First')
         return HttpResponseRedirect('/login')
 
 
 def quiz(request,id):
     global user_mobile
-    if str(user_mobile) in request.session:
-        count=0
-        if request.method=='POST':
-            questions=Question.objects.filter(course=id).values()
-            for item in questions: 
-                answer=request.POST.get(str(item['id']))
-                if str(answer)==str(item['answer']):
-                    count+=1
-            request.session[str(id)]=count
-            return redirect('result',id)
+    try:
+        if str(user_mobile) in request.session:
+            count=0
+            if request.method=='POST':
+                questions=Question.objects.filter(course=id).values()
+                for item in questions: 
+                    answer=request.POST.get(str(item['id']))
+                    if str(answer)==str(item['answer']):
+                        count+=1
+                request.session[str(id)]=count
+                request.session.set_expiry(0)
+                return redirect('result',id)
+            else:
+                questions=Question.objects.filter(course=id).values()
+            return render(request, 'questions/quiz.html',{'questions':questions})
         else:
-            questions=Question.objects.filter(course=id).values()
-        return render(request, 'questions/quiz.html',{'questions':questions})
-    else:
+            messages.warning(request,'Login First')
+            return HttpResponseRedirect('/login')
+    except NameError as e:
         messages.warning(request,'Login First')
         return HttpResponseRedirect('/login')
 
 def result(request,id):
     global user_mobile
-    if str(user_mobile) in request.session:
+    if str(id) in request.session:
         marks=request.session.get(str(id))
         return render(request,'questions/result.html',{'marks':marks})
     else:
-        messages.warning(request,'Take a quiz first')
-        return HttpResponseRedirect('/home')
+        try:
+            if str(user_mobile) in request.session:
+                messages.warning(request,'Take a quiz first')
+                return HttpResponseRedirect('/home')
+            else:
+                return HttpResponseRedirect('/home')
+        except:
+            return HttpResponseRedirect('/home')
